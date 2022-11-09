2e0cac6338416070f5655d03bc768b9bcfa927b4
dyanasys <diana.ad2209@gmail.com>
2022-11-09 01:22:47 +0100

    cambios backend

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\ArticleController;
+use App\Http\Resources\ArticleResource;
+use App\Models\Article;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,7 +16,15 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+Route::get('/article/{id}', function ($id) {
+    return new ArticleResource(Article::findOrFail($id));
+});
+Route::get('/articles', function () {
+    return ArticleResource::collection(Article::all());
+});
+Route::put('/article/{id}', [ArticleController::class, 'update']);
+Route::delete('/article/{id}', [ArticleController::class, 'destroy']);
+Route::post('/articles', [ArticleController::class, 'store']);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
