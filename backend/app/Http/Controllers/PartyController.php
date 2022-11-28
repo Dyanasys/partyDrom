7f87c830bf7dee2d3682a2a37b276c9f23b8137c
dyanasys <diana.ad2209@gmail.com>
2022-11-28 02:05:38 +0100

    query super chunga bien

--- backend/app/Http/Controllers/PartyController.php
+++ backend/app/Http/Controllers/PartyController.php
@@ -47,14 +47,14 @@ class PartyController extends Controller
     {
         $query = DB::select(
             "select p.*,
-        l.name as location_name,
-        r.id_request,
-        r.pending_request
-        FROM parties p
-        join locations l on p.id_location = l.id
-        LEFT JOIN (SELECT rr.id AS id_request, rr.pending AS pending_request, rr.id_user AS id_user_request, rr.id_party AS id_party_request from requests rr right JOIN parties pp ON rr.id_party = pp.id WHERE rr.id_user <> $id_user ORDER BY rr.pending DESC LIMIT 1) r
-        ON r.id_party_request = p.id
-        where p.id_user = $id_user"
+            l.name as location_name,
+            r.id_request,
+            r.pending_request
+            FROM parties p
+            join locations l on p.id_location = l.id
+            LEFT JOIN (SELECT rr.id AS id_request, sum(rr.pending) AS pending_request, rr.id_user AS id_user_request, rr.id_party AS id_party_request from requests rr right JOIN parties pp ON rr.id_party = pp.id WHERE rr.id_user <> $id_user GROUP BY rr.id_user, rr.id, rr.id_party ) r
+            ON r.id_party_request = p.id
+            where p.id_user = $id_user;"
         );
 
         return $query;
